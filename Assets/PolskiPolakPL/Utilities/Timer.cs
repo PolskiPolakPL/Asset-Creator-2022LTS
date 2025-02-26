e3792176028ddbd35e530632d5bc3544cc2fba78
PolskiPolakPL <97120832+PolskiPolakPL@users.noreply.github.com>
2025-02-26 17:24:24 +0100

    Added OnTimerTick action
    - Added `Timer.OnTimerTick` Action that is invoked every tick
    - Added `OnDurationChanged` Action
    - Changed Constructor override to include a parameter with default value
    - Added public method `ChangeDuration()` that takes new duration and by default resets time
      - Added method variant that does not invoke `OnDurationChanged` Action.
    - Added optional parameter for `Ticj()` method that checks if `OnTimerTick` Action is invoked

--- Assets/PolskiPolakPL/Utilities/Timer.cs
+++ Assets/PolskiPolakPL/Utilities/Timer.cs
@@ -1,9 +1,9 @@
-using System; 
+using System;
 namespace PolskiPolakPL.Utils
 {
     /// <summary>
-    /// Timer class from tutorial extended by PolskiPolakPL. Tutorial link: 
-    /// <seealso href="https://youtu.be/pRjTM3pzqDw"></seealso>
+    /// Timer class from tutorial extended by PolskiPolakPL. You can find original tutorial
+    /// <seealso href="https://youtu.be/pRjTM3pzqDw">here</seealso>
     /// </summary>
     public class Timer
     {
@@ -35,9 +35,23 @@ namespace PolskiPolakPL.Utils
 
 
         /// <summary>
-        /// Timer Action Event invoked at the end of counting time.
+        /// Timer Action event invoked at the end of counting time.
         /// </summary>
         public event Action OnTimerElapsed;
+
+
+
+        /// <summary>
+        /// Timer Action event invoked every <c>Tick()</c>.
+        /// </summary>
+        public event Action OnTimerTick;
+
+
+
+        /// <summary>
+        /// Timer Action event invoked whet it's duration is changed.
+        /// </summary>
+        public event Action OnDurationChanged;
 
 
 
@@ -45,25 +59,44 @@ namespace PolskiPolakPL.Utils
 
 
 
+        /// <summary>
+        /// Constructor for Timer class.
+        /// </summary>
         /// <param name="duration">Duration of the timer in seconds</param>
-        public Timer(float duration)
+        /// <param name="isLooping">Controlls if Timer is looping. Default = <c>true</c></param>
+        public Timer(float duration, bool isLooping = true)
         {
             this.duration = duration;
             RemaningSeconds = duration;
+            this.isLooping = isLooping;
+        }
+
+
+        /// <summary>
+        /// Changes base duration of the Timer.
+        /// </summary>
+        /// <param name="newDuration">sets new duration</param>
+        /// <param name="resetCurrentTime">reserts remaning time back to the beginning. Default = <c>true</c></param>
+        public void ChangeDuration(float newDuration, bool resetCurrentTime = true)
+        {
+            duration = newDuration;
+            if (resetCurrentTime)
+                RemaningSeconds = duration;
+            OnDurationChanged?.Invoke();
         }
 
 
 
         /// <summary>
-        /// 
+        /// Changes base duration of the Timer. Doesn't invoke <c>OnDurationChanged</c> action.
         /// </summary>
-        /// <param name="duration">Duration of the timer in seconds</param>
-        /// <param name="isLooping">Does timer loop over time?</param>
-        public Timer(float duration, bool isLooping)
+        /// <param name="newDuration"></param>
+        /// <param name="resetCurrentTime"></param>
+        public void ChangeDurationWithoutNotify(float newDuration, bool resetCurrentTime = true)
         {
-            this.duration = duration;
-            RemaningSeconds = duration;
-            this.isLooping = isLooping;
+            duration = newDuration;
+            if (resetCurrentTime)
+                RemaningSeconds = duration;
         }
 
 
@@ -72,7 +105,8 @@ namespace PolskiPolakPL.Utils
         /// Method used to move time one tick. Recommended use in <c>Update()</c> or <c>FixedUpdate()</c> methods.
         /// </summary>
         /// <param name="deltaTime">time difference between ticks</param>
-        public void Tick(float deltaTime)
+        /// <param name="invokeEvent">Controlls if Timer invokes <c>OnTimerChanged</c> Action. Deafault = <c>false</c></param>
+        public void Tick(float deltaTime, bool invokeEvent = false)
         {
             if(RemaningSeconds == 0)
             {
@@ -82,6 +116,8 @@ namespace PolskiPolakPL.Utils
             }
             RemaningSeconds -= deltaTime;
             SecondsPassed += deltaTime;
+            if(invokeEvent)
+                OnTimerTick?.Invoke();
             CheckForTimerEnd();
         }
 
