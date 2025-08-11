bc6fa9c79de28a08df93f9fa8e152ef5e9e91b8b
PolskiPolakPL <97120832+PolskiPolakPL@users.noreply.github.com>
2025-08-11 20:48:22 +0200

    App Manager part 1
    - Created New Scene
    - Created AppManager
    - Added SetCursorLockState()
    - Added SetFPSLimit()
    - Created Simple FPS Counter

---
+++ Assets/PolskiPolakPL/Temporary/FPSCounter.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using PolskiPolakPL.Utils;
+using TMPro;
+
+public class FPSCounter : MonoBehaviour
+{
+    [SerializeField] float refreshTime = 0.2f;
+    TMP_Text fpsTextField;
+    Timer timer;
+
+    int currentFPS = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fpsTextField = GetComponent<TMP_Text>();
+        timer = new Timer(refreshTime);
+        timer.OnTimerElapsed += RefreshCounter;
+        timer.Tick(refreshTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer.Tick(Time.deltaTime);
+    }
+    void MeasureFPS()
+    {
+        currentFPS = Mathf.RoundToInt(1/Time.deltaTime);
+        Debug.Log(currentFPS);
+    }
+
+    void RefreshCounter()
+    {
+        MeasureFPS();
+        fpsTextField.text = currentFPS.ToString();
+        Debug.Log(currentFPS.ToString());
+    }
+
+    private void OnDestroy()
+    {
+        timer.OnTimerElapsed -= RefreshCounter;
+    }
+}
---
+++ Assets/PolskiPolakPL/Utilities/App Manager/AppManager.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class AppManager : MonoBehaviour
+{
+    public static AppManager Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance && Instance != this)
+            Destroy(this);
+        else
+            Instance = this;
+    }
+
+    [Min(1)]
+    [SerializeField] int minFPSCount = 60;
+
+
+    public void SetCursorLockState(int lockStateID)
+    {
+        switch (lockStateID)
+        {
+            case 0:
+                {
+                    Cursor.lockState = CursorLockMode.None;
+                }break;
+
+            case 1:
+                {
+                    Cursor.lockState = CursorLockMode.Locked;
+                }
+                break;
+
+            case 2:
+                {
+                    Cursor.lockState = CursorLockMode.Confined;
+                }
+                break;
+
+            default:
+                {
+                    Debug.LogWarning($"LockStateID = {lockStateID} is invalid!");
+                }
+                break;
+        }
+    }
+
+    public void SetFPSLimit(float value)
+    {
+        if (value < minFPSCount)
+        {
+            Debug.LogWarning($"FPS limit cannot be below {minFPSCount}! You can change required minimum value in AppManager Instance.");
+            return;
+        }
+        Application.targetFrameRate = Mathf.RoundToInt(value);
+    }
+
+
+}
