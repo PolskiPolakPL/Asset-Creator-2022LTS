8f21685a38222796489da4708bdc22f19e978677
PolskiPolakPL <97120832+PolskiPolakPL@users.noreply.github.com>
2026-05-05 12:22:01 +0200

    ItemSlot Refactorization
    - Added 'ItemAmount' TextField to `itemSlot` Prefab
    - Added fields and methods according to tutorial
    - Following tutorial pt 1.

--- Assets/PolskiPolakPL/Inventory System/UI/ItemSlot.cs
+++ Assets/PolskiPolakPL/Inventory System/UI/ItemSlot.cs
@@ -1,19 +1,35 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ItemSlot : MonoBehaviour
+public class ItemSlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+
+    public bool hovering {  get; private set; }
+
+    // Item Data
     [SerializeField] ItemData heldItem;
+    int itemAmount;
+
+    // UI Elements
     [SerializeField] RawImage itemRawImage;
+    [SerializeField] TMP_Text amountTextField;
 
     public ItemData GetItemData()
     {
         return heldItem;
     }
 
-    public void SetItem(ItemData item)
+    public int GetAmount()
+    {
+        return itemAmount;
+    }
+
+    public void SetItem(ItemData item, int amount = 1)
     {
         heldItem = item;
+        itemAmount = amount;
         itemRawImage.texture = item.imageArray;
         itemRawImage.uvRect = item.UVRect;
         UpdateSlot();
@@ -24,16 +40,40 @@ public class ItemSlot : MonoBehaviour
         if (HasItem())
         {
             itemRawImage.enabled = true;
+            if (itemAmount > 1)
+                amountTextField.text = itemAmount.ToString();
+            else
+                amountTextField.text = "";
         }
         else
         {
             itemRawImage.enabled = false;
+            amountTextField.text = "";
         }
+    }
+
+    public int AddAmount(int amount)
+    {
+        itemAmount += amount;
+        UpdateSlot();
+        return itemAmount;
+    }
+
+    public int RemoveAmount(int amount)
+    {
+        itemAmount -= amount;
+        if(itemAmount <= 0)
+            ClearSlot();
+        else
+            UpdateSlot();
+
+        return itemAmount;
     }
 
     public void ClearSlot()
     {
         heldItem = null;
+        itemAmount = 0;
         UpdateSlot();
     }
 
@@ -42,4 +82,13 @@ public class ItemSlot : MonoBehaviour
         return heldItem != null;
     }
 
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        hovering = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        hovering = false;
+    }
 }
